16cbbf2b1f2fc1e81f46c78199a9feb99a63506f
Abhijeet <ranarahul16.rr@gmail.com>
2025-06-25 01:43:56 +0530

    changes F

--- components/listings/GridPostGallery.tsx
+++ components/listings/GridPostGallery.tsx
@@ -12,13 +12,26 @@ export default function GridPostGallery({ posts }: Props) {
   return (
     <div className="grid grid-cols-3 gap-[2px]">
       {posts.map((post) => (
-        <div key={post.id} className="relative aspect-square overflow-hidden">
-          <Image
-            src={post.media_url}
-            alt='Instagram post'
-            fill
-            className="object-cover"
-          />
+        <div key={post.id} className="relative aspect-square overflow-hidden bg-black">
+          {post.media_type === 'IMAGE' || post.media_type === 'CAROUSEL_ALBUM' ? (
+            <Image
+              src={post.media_url}
+              alt="Instagram post"
+              fill
+              className="object-cover"
+              unoptimized // needed if image is not from allowed domain
+            />
+          ) : post.media_type === 'VIDEO' ? (
+            <video
+              src={post.media_url}
+              className="object-cover w-full h-full"
+              autoPlay
+              muted
+              loop
+              playsInline
+            />
+          ) : null}
+
           {post.media_type === 'VIDEO' && (
             <div className="absolute top-1 right-1">
               <Image
