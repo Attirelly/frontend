7fea37da95bb02068cfea481322f2bfac69b3901
geeky-AB <77844897+geeky-AB@users.noreply.github.com>
2025-07-03 17:13:04 +0530

    latest frontend changes

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -1,4 +1,5 @@
 import CardTypeOne from "@/components/homepage/CardTypeOne";
+import CardTwoType from "@/components/homepage/CardTypeTwo";
 import HeroSection from "@/components/homepage/HeroSection";
 import StoreTypeSelection from "@/components/homepage/StoreTypeSelection";
 import ListingPageHeader from "@/components/listings/ListingPageHeader";
@@ -11,11 +12,15 @@ export default function HomePage() {
             <div className="mt-16 mx-auto mb-16">
                 <StoreTypeSelection />
             </div>
-            <div>HI</div>
+            <div className="mx-auto">
+               <CardTwoType imageurl="/Homepage/CardTypeTwo.svg" title="Label Parampara by Archit" description="Modal Town, Ludhiana"/>
+            </div>
 
             <div className="mx-auto">
                 <CardTypeOne imageUrl="/Homepage/CardTypeOne.svg" discountText="23" title="Embroidary Kurta"/>
             </div>
+
+
 
         </div>
     )
