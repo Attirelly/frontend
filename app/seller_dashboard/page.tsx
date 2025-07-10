4c4fba088bd1872c9d327984a5fef4ad46c5c45b
Abhijeet <ranarahul16.rr@gmail.com>
2025-07-10 22:47:54 +0530

    f c

--- app/seller_dashboard/page.tsx
+++ app/seller_dashboard/page.tsx
@@ -243,6 +243,7 @@
 
 import SellerDashboardContainer from "@/components/Seller/SellerDashboardContainer";
 import { Suspense } from "react";
+import {useRouter} from "next/navigation";
 
 export default function SellerDashboardPage(){
   return (
