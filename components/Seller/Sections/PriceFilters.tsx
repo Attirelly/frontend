ac9b5e8d7c93565a0b090a9e480de9107cfc1326
Abhijeet <ranarahul16.rr@gmail.com>
2025-07-19 01:58:14 +0530

    fe changes

--- components/Seller/Sections/PriceFilters.tsx
+++ components/Seller/Sections/PriceFilters.tsx
@@ -312,7 +312,7 @@ export default function PriceFiltersComponent() {
         priceRanges: selectedPrices,
         priceRangesStr: selectedPrices.map((item) => ({
           id: item.price_range_id,
-          label: item.price_range,
+          label: item.price_range ?? "",
         })),
       });
     }
