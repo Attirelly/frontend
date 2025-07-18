3ced9e6866eb4b31e2dee0f28ee374db0e6bd69c
Abhijeet <ranarahul16.rr@gmail.com>
2025-07-18 22:52:09 +0530

    fe chages

--- components/listings/StoreInfoHeader.tsx
+++ components/listings/StoreInfoHeader.tsx
@@ -54,7 +54,7 @@ export default function StoreInfoPage({
         <div className="grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-6 p-6">
             {/* Left: Circular Store Image */}
             {/* <div className="flex justify-center items-center w-76.5 h-63.75 bg-[#F8F8F8] p-4 rounded-2xl"> */}
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center justify-center">
                     <div className="relative w-32 h-32">
                         <Image
                             src={imageUrl}
