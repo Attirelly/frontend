266e28177f1b6132bbeab431709b57d324a5fed9
Abhijeet <ranarahul16.rr@gmail.com>
2025-07-19 00:17:39 +0530

    fe changes

--- components/Seller/Sections/ViewAllProducts.tsx
+++ components/Seller/Sections/ViewAllProducts.tsx
@@ -123,8 +123,9 @@ export default function ProductsPage({
 
 
   const handleImageUpload = (record: Product) => {
-    message.success(`Image uploaded for ${record.product_name}`);
+    // message.success(`Image uploaded for ${record.product_name}`);
     actions.setCurrentStep(6);
+    window.open(`/product_upload/${record.product_id}`, '_blank', 'noopener,noreferrer');
   };
 
   const columns = [
