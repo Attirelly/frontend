7ca7295550f2a50124c295e8c560b90ddef90b1e
amangcod3 <amangcod3@gmail.com>
2025-08-21 21:16:21 +0530

    final fixed

--- app/store_profile/[storeId]/StoreProfilePage.tsx
+++ app/store_profile/[storeId]/StoreProfilePage.tsx
@@ -8,17 +8,21 @@ import PostGalleryContainer from "@/components/listings/PostsContainer";
 import ListingFooter from "@/components/listings/ListingFooter";
 import Catalogue from "@/components/listings/Catalogue";
 import DynamicFilter from "@/components/listings/DynamicFilter";
-import { useParams, usePathname, useRouter, useSearchParams } from "next/navigation";
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
 import { useProductFilterStore } from "@/store/filterStore";
 import ProductContainer from "@/components/listings/ProductContainer";
 import SortByDropdown from "@/components/listings/SortByDropdown";
 
 export default function StoreProfilePage() {
-
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
-    const {
+  const {
     setQuery,
     query,
     setStoreType,
@@ -30,9 +34,10 @@ export default function StoreProfilePage() {
     setCity,
     storeType,
     allStoreType,
-    viewType
+    viewType,
   } = useHeaderStore();
-  const { setFacetInit, setFacets, setPriceBounds, setPriceRange } = useProductFilterStore();
+  const { initializeFilters, selectedFilters, selectedPriceRange , setFacetInit }  = useProductFilterStore();
+
   const [showFilters, setShowFilters] = useState(false);
 
   const params = useParams();
@@ -41,18 +46,8 @@ export default function StoreProfilePage() {
   const defaultButton = searchParams.get("defaultButton");
 
   useEffect(() => {
-  setFacetInit(false);
-  setPriceRange([0,0]);
-}, []);
-
-
-  const {
-    initializeFilters,
-    selectedFilters,
-    selectedPriceRange
-  } = useProductFilterStore();
-
- 
+    setFacetInit(false);
+  }, []);
 
   //  initialise the state using url
   useEffect(() => {
@@ -69,8 +64,9 @@ export default function StoreProfilePage() {
         key !== "sortBy" &&
         key !== "price" &&
         key !== "city" &&
-        key !== "area" && 
-        key !== "store_type"
+        key !== "area" &&
+        key !== "store_type"&&
+        key !== "defaultButton"
       ) {
         initialSelectedFilters[key] = value.split(",");
       }
@@ -110,7 +106,6 @@ export default function StoreProfilePage() {
       selectedFilters: initialSelectedFilters,
       priceRange: initialPriceRange,
     });
-
   }, [searchParams, initializeFilters, setQuery, setStoreType]);
 
   useEffect(() => {
@@ -149,116 +144,15 @@ export default function StoreProfilePage() {
     }
 
     router.replace(`${pathname}?${newparams.toString()}`);
-  }, [selectedFilters, selectedPriceRange, pathname, city, area, storeType, router]);
-
-
-
-  const {
-    initializeFilters,
+  }, [
     selectedFilters,
-    selectedPriceRange
-  } = useProductFilterStore();
-
- 
-
-  //  initialise the state using url
-  useEffect(() => {
-    const params = new URLSearchParams(searchParams);
-    const initialSelectedFilters: Record<string, string[]> = {};
-    const search = params.get("search") || "";
-    const cityName = params.get("city");
-    const areaName = params.get("area");
-    const storeTypeName = params.get("store_type");
-
-    params.forEach((value, key) => {
-      if (
-        key !== "search" &&
-        key !== "sortBy" &&
-        key !== "price" &&
-        key !== "city" &&
-        key !== "area" && 
-        key !== "store_type"
-      ) {
-        initialSelectedFilters[key] = value.split(",");
-      }
-    });
-
-    // Only perform the lookup if the master lists have been loaded
-    if (allCity && allCity.length > 0 && cityName) {
-      const cityObject = allCity.find((c) => c.name === cityName);
-      console.log("url_city", cityObject);
-      if (cityObject) setCity(cityObject);
-    }
-
-    if (allArea && allArea.length > 0 && areaName) {
-      const areaObject = allArea.find((a) => a.name === areaName);
-      console.log(areaObject);
-      if (areaObject) setArea(areaObject);
-    }
-    let initialPriceRange: [number, number] | null = null;
-    const priceParam = params.get("price");
-    if (priceParam) {
-      const [min, max] = priceParam.split("-").map(Number);
-      if (!isNaN(min) && !isNaN(max)) {
-        initialPriceRange = [min, max];
-      }
-    }
-    if (storeTypeName) {
-      const storeTypeObject = allStoreType.find(
-        (st) => st.store_type === storeTypeName
-      );
-      if (storeTypeObject) {
-        setStoreType(storeTypeObject);
-      }
-    }
-
-    setQuery(search);
-    initializeFilters({
-      selectedFilters: initialSelectedFilters,
-      priceRange: initialPriceRange,
-    });
-
-  }, [searchParams, initializeFilters, setQuery, setStoreType]);
-
-  useEffect(() => {
-    const oldparams = new URLSearchParams(searchParams);
-    const newparams = new URLSearchParams();
-    if (query) {
-      newparams.set("search", query);
-    }
-    if (oldparams.get("categories")) {
-      newparams.set("categories", oldparams.get("categories") || "");
-    }
-    // if (sortBy) {
-    //   params.set("sortBy", sortBy);
-    // }
-    console.log("select filter", selectedFilters);
-    console.log("city and area", city, area);
-    Object.entries(selectedFilters).forEach(([key, values]) => {
-      if (values && values.length > 0) {
-        newparams.set(key, values.join(","));
-      }
-    });
-
-    if (city) {
-      newparams.set("city", city.name);
-    }
-    if (area) {
-      newparams.set("area", area.name);
-    }
-
-    if (selectedPriceRange) {
-      const [min, max] = selectedPriceRange;
-      newparams.set("price", `${min}-${max}`);
-    }
-    if (storeType) {
-      newparams.set("store_type", storeType.store_type);
-    }
-
-    router.replace(`${pathname}?${newparams.toString()}`);
-  }, [selectedFilters, selectedPriceRange, pathname, city, area, storeType, router]);
-
-
+    selectedPriceRange,
+    pathname,
+    city,
+    area,
+    storeType,
+    router,
+  ]);
 
   return (
     <div className="flex flex-col bg-[#FFFFFF]">
