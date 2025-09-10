c605824bdc8a1f11db21e98b8f9f440691f10b33
Abhijeet <ranarahul16.rr@gmail.com>
2025-09-10 11:45:48 +0530

    dropdown and store type center fixed

--- app/product_directory/ProductListPage.tsx
+++ app/product_directory/ProductListPage.tsx
@@ -267,7 +267,7 @@ export default function ProductListPage() {
           </div>
         </div>
 
-        <div className="md:hidden fixed bottom-0 left-0 right-0 bg-white p-2 shadow-[0_-2px_10px_rgba(0,0,0,0.1)] z-30 flex justify-center">
+        <div className="md:hidden fixed bottom-0 left-0 right-0 bg-white p-2 shadow-[0_-2px_10px_rgba(0,0,0,0.1)] z-30 flex justify-center items-center">
           <StoreTypeTabs
             defaultValue={storeType?.store_type ?? matchedStoreType ?? ""}
             context="products"
