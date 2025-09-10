c26a480f4f404f40539a20d27bde10edd1a415ac
Abhijeet <ranarahul16.rr@gmail.com>
2025-09-11 00:29:25 +0530

    fe changes

--- components/listings/MenWomenNavbar.tsx
+++ components/listings/MenWomenNavbar.tsx
@@ -45,6 +45,7 @@ export default function MenWomenNavbar() {
     null
   );
   const { setQuery } = useHeaderStore();
+  console.log();
 
   useEffect(() => {
     const fetchCategories = async () => {
